978fd5fce69c2eee0c8997815c8c86e1265e75a1
MatheusBigode <99267136+MatheusBigode@users.noreply.github.com>
2022-05-03 12:58:11 -0300

    Add files via upload

---
+++ Matheus Santana/atividades/atividade2.js
@@ -0,0 +1,7 @@
+let n = 100
+
+for (i = 0; i < n; i++){
+    if(i%2==0)
+   console.log("Numeros pares", i+2)
+}
+
---
+++ Matheus Santana/atividades/atividade4.js
@@ -0,0 +1,8 @@
+
+numero = 15000.00
+n = 5
+
+for(i = 0; i < n; i++){
+    console.log(numero)
+    numero /=  2
+}
